d0a321ddc805e1faf1067d85562e771be4cd01bb
kenmueller <kenmueller0@gmail.com>
2021-02-28 17:43:01 -0800

    accept requests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+# columns: [...questions]
+# responses: { 'question': [...responses], 'outcome': [...outcomes] }
+# response: { 'question': [response] }
+
+from flask import Flask, request
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def index():
-    return 'asadvice'
+	data = request.json
+	
+	pima = pd.DataFrame(data.responses)
+	
+	x = pima[data.columns]
+	y = pima.outcome
+
+	clf = DecisionTreeClassifier(criterion='entropy', max_depth=3).fit(x, y)
+	y_pred = clf.predict(pd.DataFrame(data.response))
+
+	return y_pred[0]
+
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+	app.run(threaded=True, port=5000)
